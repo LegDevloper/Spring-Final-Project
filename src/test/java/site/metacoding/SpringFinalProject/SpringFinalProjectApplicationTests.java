855344db2dbb6f0a0dd467c9835effbdd5e0a11f
Choi JaeYoung <chlwowudd0327@naver.com>
2022-09-23 13:38:52 +0900

    Project Setting

---
+++ src/test/java/site/metacoding/SpringFinalProject/SpringFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package site.metacoding.SpringFinalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
